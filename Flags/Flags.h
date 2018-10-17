7d9fc18c40d4158384dc6a4c64fdc8afb4edf1fb
Cruz <cruzdiary@gmail.com>
2018-10-18 00:20:58 +0900

    Support Carthage

---
+++ Flags/Flags.h
@@ -0,0 +1,19 @@
+//
+//  Flags.h
+//  Flags
+//
+//  Created by Cruz on 18/10/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Flags.
+FOUNDATION_EXPORT double FlagsVersionNumber;
+
+//! Project version string for Flags.
+FOUNDATION_EXPORT const unsigned char FlagsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flags/PublicHeader.h>
+
+
