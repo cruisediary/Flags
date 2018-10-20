a5acdf93b6e40ea75fdb2d02e7747fdc3a3978e9
Cruz <cruzdiary@gmail.com>
2018-10-20 10:47:39 +0900

    Test FlagSpec

---
+++ FlagsTests/FlagSpec.swift
@@ -0,0 +1,60 @@
+//
+//  FlagSpec.swift
+//  FlagSpec
+//
+//  Created by Cruz on 18/10/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+
+@testable import Flags
+class FlagSpec: QuickSpec {
+    
+    override func spec() {
+        super.spec()
+        var sut: Flag?
+        
+        describe("Flag") {
+            for countryCode in NSLocale.isoCountryCodes {
+                context("when country code is \(countryCode)") {
+                    beforeEach {
+                        sut = Flag(countryCode: countryCode)
+                    }
+                    
+                    it("should not be nil") {
+                        expect(sut).toNot(beNil())
+                    }
+                    
+                    it("emoji should not be nil") {
+                        expect(sut?.emoji).toNot(beNil())
+                    }
+                    
+                    it("image should not be nil") {
+                        expect(sut?.image).toNot(beNil())
+                    }
+                }
+                
+                context("when country code is lowercased \(countryCode.lowercased())") {
+                    beforeEach {
+                        sut = Flag(countryCode: countryCode.lowercased())
+                    }
+                    
+                    it("should not be nil") {
+                        expect(sut).toNot(beNil())
+                    }
+                    
+                    it("emoji should not be nil") {
+                        expect(sut?.emoji).toNot(beNil())
+                    }
+                    
+                    it("image should not be nil") {
+                        expect(sut?.image).toNot(beNil())
+                    }
+                }
+            }
+        }
+    }
+}
