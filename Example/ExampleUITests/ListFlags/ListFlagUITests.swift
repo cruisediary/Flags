ef6403f5d0eb6c2c3217b01a7a756abef63c1de0
Cruz <cruzdiary@gmail.com>
2018-11-04 18:25:44 +0900

    Add SearchFieldExists UITest case

---
+++ Example/ExampleUITests/ListFlags/ListFlagUITests.swift
@@ -0,0 +1,15 @@
+//
+//  ListFlagUITests.swift
+//  ExampleUITests
+//
+//  Created by Cruz on 04/11/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import XCTest
+
+class ListFlagUITests: UITestCase {
+    func testSearchFieldExists() {
+        XCTAssertTrue(app.searchFields.firstMatch.exists)
+    }
+}
