1b8f1aed1da4e6eaa1a326876aa7088951aa2248
Cruz <cruzdiary@gmail.com>
2018-11-12 00:20:54 +0900

    Add ListFlagsReactorSpec to test empty typing

---
+++ Example/ExampleTests/ListFlags/ListFlagsReactorSpec.swift
@@ -0,0 +1,35 @@
+//
+//  ListFlagsReactorSpec.swift
+//  ExampleTests
+//
+//  Created by Cruz on 11/11/2018.
+//  Copyright © 2018 Cruz. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+
+@testable import Example
+class ListFlagsReactorSpec: QuickSpec {
+    override func spec() {
+        super.spec()
+        var sut: ListFlagsReactor!
+        beforeEach {
+            sut = ListFlagsReactor()
+        }
+
+        describe("ListFlagsReactor") {
+            context("when empty typing action") {
+                beforeEach {
+                    sut.action.onNext(.typing(text: ""))
+                }
+
+                it("countryCodes count should greater than 254") {
+                    expect(sut.currentState.countryCodes.count)
+                        .toEventually(beGreaterThan(254))
+                }
+            }
+        }
+    }
+}
